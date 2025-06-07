c920c0d06580dfad4e5cdd59b0af0c7a586f9e01
Cynthia Rattey <20534887@fhda.edu>
2025-06-07 13:25:24 -0700

    Update StarbucksStore.java

--- StarbucksStore.java
+++ StarbucksStore.java
@@ -6,56 +6,62 @@ package edu.foothill.cs1a.finalexam;
 //      store location information and vote information.
 public class StarbucksStore
 {
-    // TODO: Define the following attributes with "private" visibility:
-    // "storeName" of type String, which is the name of the store
-    // "location" of type String, which is the city location for the store
-    // "state" of type String, which is the state the store is in
-    // "RTWstate" of type boolean, which specifies whether the store is in a Right
-    // Work State
-    // "votedYes" of type int, which is the number of "yes" votes
-    // "votedNo" of type int, which is the number of "no" votes
-    // "result" of type boolean, which specifies whether to there were enough yes
-    // votes to enable employees to unionize.
-    private boolean result;
-    private boolean RTWstate;
-    private int votedNo;
-    private int votedYes;
-    private String location;
-    private String city;
-    private String state;
+    // 
+    // @param: "storeName" of type String, which is the name of the store
+    // @param: "location" of type String, which is the city location for the store
+    // @param: "state" of type String, which is the state the store is in
+    // @param: "RTWstate" of type boolean, which specifies whether the store is in a Right
+    // @param: Work State
+    // @param: "votedYes" of type int, which is the number of "yes" votes
+    // @param: "votedNo" of type int, which is the number of "no" votes
+    // @param: "result" of type boolean, which specifies whether to there were enough yes
+    // @param: votes to enable employees to unionize.
+    // I took a small data visualization class through google that said it is proper to put "@param" when finishing to do lists it makes
+    // for easer to read code. As requested, each variable has been set to private with the proper setting of string boolean or int. 
     private String storeName;
+    private String location;
+    private String state;
+    private boolean RTWstate;
+    private int votedYes;
+    private int votedNo;
+    private boolean results;
     
-   
+    
+
     // TODO: complete method signature to receive an argument for each attribute as
     // required by test class called TestStarBucks.
     // Should construct an object of type StarbuckStore by initializing the instance
     // variables and checks whether the result to unionize is true only if the
     // number of employees who voted Yes is more than the number of employees
     // who voted No.
-    public void StarbucksStore(String storeName)
+    /* @param: initialized the above variables to the proper setting
+     * This will set  */
+    public StarbucksStore(String storeName, String location, String state, boolean RTWstate,
+            int votedYes, int votedNo, boolean results)
     {
-        String city;
-        // TODO: Initialize attributes
-        storeName = this.storeName;
-        location = city + state;
-        state = this.state;
-        votedYes = this.votedYes;
-        votedNo = this.votedNo;
-        if(votedYes > votedNo)
-            {
-                result = true;
-                System.out.print(result);
-            }
-        
-        // TODO: Use conditional statement to validate that the result is only false
-        // if the number of No votes is greater than or equal to the number of Yes
-        // votes, otherwise print a message similar to the sample output.
-        if (votedNo > votedYes)
-        {
-            result = false;
-            System.out.print(result);
-        }
+        /* @param: Initialized variables set to proper positioning */
 
+        this.storeName = storeName;
+        this.location =  location;
+        this.state = state;
+        this.RTWstate = RTWstate;
+        this.votedYes = votedYes;
+        this.votedNo = votedNo;
+        results = (this.votedYes < this.votedNo);
+        
+        this.results = (this.votedYes > this.votedNo);
+        
+        
+        /* The bottom variable has been initiated with the @param: results will only be valid  if Voted yes is
+         * greater than votedNo */ 
+        if (this.votedYes > this.votedNo)
+        {
+            System.out.println(storeName + " Unionize Passed and is a RTWstate! ");
+        } else 
+            if (this.votedNo >= this.votedYes)
+            {
+                System.out.println (storeName + " Did not pass Unization.");
+            }
     }
 
     // TODO: complete constructor such that it receives one argument of type String
@@ -66,50 +72,65 @@ public class StarbucksStore
     // the hard-coded String "unknown" for the location and state attributes
     // false for the RTWstate and result attributes
     // the hard-coded value 0 (i.e. zero) for the votedYes and votedNo attributes
+    
     public String storeName(String storeName)
     {
-        this.StarbucksStore(storeName, "", "", false,
-            0, 0, true);
+        /* @param: Initiated variables to required settings for default, leaving store name the only attribute available  */
+        storeName = this.storeName;
+        location = "unknown";
+        state = "unknown";
+        RTWstate = false;
+        this.votedYes = 1;
+        this.votedNo = 0;
+        results = false;
+        
+        return storeName;
     }
-    public void StarbucksStore(String storeName, String location, String state, boolean RTWstate,
-            int votedYes, int votedNo, boolean result)
+
+    public  StarbucksStore(String storeName)
     {
+        /* constructor method for grabbing store by Store Name. */
         // TODO: Initialize attribute by calling the constructor above using this()
         // method call passing in
         // the values described in the TODO method description
-        this.StarbucksStore(storeName, location, state, RTWstate, votedYes, votedNo, result);
+        this(storeName, "Unknown", "Unknown", false, 0, 0, true);
     }
 
     // TODO: Define a getter (i.e. accessor) method for storeName attribute
     // to receive zero arguments and to return a value of type String.
     public String getStoreName()
     {
-        this.storeName(storeName);
-        return storeName;
+        /* This gets only the store name using object orientated programming referencing another method in this class. */
+       return this.storeName(storeName);
+
     }
+
     // TODO: Define a getter (i.e. accessor) method for stateName attribute
     // to receive zero arguments and to return a value of type String.
     public String getStateName()
     {
-        this.StarbucksStore("unknown", "unknown", state, false, 0, 0, true);
-        return state;
+        /* This method is to get the State Name in abbreviations */
+        
+        return this.state;
+
     }
 
     // TODO: Define a getter (i.e. accessor) method for RTWstate attribute
     // to receive zero arguments and to return a value of type boolean.
     public boolean getRTWstate()
     {
-        this.StarbucksStore("unknown", "unknown", "unknown", RTWstate, 0, 0, true);
+        /* This method retrieves the Right to work states. @param: No arguments, and returns boolean */
         
-        return RTWstate;
+        return this.RTWstate;
     }
+
     // TODO: Define a getter (i.e. accessor) method for getVoteYes attribute
     // to receive zero arguments and to return a value of type int (the number of
     // Yes votes).
     public int getVotedYes()
     {
-        this.StarbucksStore("unknown", "unknown", "unknown", false, votedYes, 0, true);
-        return votedYes;
+        /* This method retrieves the Yes Votes, @param: 0 arguments and returns an int */
+        return this.votedYes;
     }
 
     // TODO: Define a getter (i.e. accessor) method for getVoteNo attribute
@@ -117,60 +138,63 @@ public class StarbucksStore
     // votes).
     public int getVotedNo()
     {
-        this.StarbucksStore("unknown", "unknown", "", false, 0, votedNo, true);
-        return votedNo;
+        /*This gets all the votedNo results. @param: 0 arguments returns int */
+        return this.votedNo;
     }
 
     // TODO: Define a getter (i.e. accessor) method for result attribute
     // to receive zero arguments and to return a value of type boolean.
     public boolean getVoteResult()
     {
-        if (votedYes > votedNo)
-        {
-            this.StarbucksStore("unknown", "unknown", "unknown", false, votedYes, votedNo, result);
-            result = this.result;
-        }
-        return result;
+        /*This method gets the vote results, @param: 0 arguments and returns boolean*/
+        return this.results;
     }
 
     // TODO: Define a setter (i.e. mutator) method for storeName attribute
     // to receive an argument of type String,
     // and to return nothing.
-    private String applyStoreName(String storeName)
+    public void setStoreName(String storeName)
     {
-        if (storeName.length() < 1)
+        /* This method sets the store name if the conditions of not being null and has not been trimmed or is empty
+         * upon finding that one is it sends out an error message. @param: attribute Store Name and returns nothing */
+        if (storeName != null && !storeName.trim().isEmpty())
         {
-            System.out.println("Error, your Store Name cannot be found");
+            this.storeName = storeName;
         } else
-            if (storeName.length() > 1)
-            {
-              storeName = this.storeName(storeName); 
-            }
-        
+        {
+            System.out.println("Error, Store Name is empty or doesn't exist");
+        }
     }
 
     // TODO: Define a setter (i.e. mutator) method for location attribute
     // to receive an argument of type String,
     // and to return nothing.
-    private String applyLocaion(String location)
+    public void setStoreLocation(String location)
     {
-        if (location.length() > 1)
+        /* Sets the location if the same conditions above in much the same way. Sets location to its actual attribute if
+         * conditions have been met. if not an error message has been pushed @param: location for argument set to string and
+         * returns nothing. */
+        if (location != null && !location.trim().isEmpty())
         {
-            this.StarbucksStore("", location, "", false, 0, 0, true);
-            location = city + state;
+            this.location = location;
         } else
-            if(location.length() < 1)
-            {
-                System.out.println("Error, your location cannot be found. "); 
-            }
-        
+        {
+            System.out.println("Error, location is empty or doesn't exist");
+        }
+
     }
+
     // TODO: Define a method called printStore which receives zero
     // arguments and does not return anything.
     // Prints to standard out a String representation of the instance including
     // the storeName, the location and the result of voting to unionize.
     public void printStore()
     {
-        this.StarbucksStore(storeName, location, state, RTWstate, votedYes, votedNo, result);
+        /*This method pulls Store Name Location and State name in the location and whether Unionize, using System out. I also used the
+         * object oriented programming using "this".*/
+        System.out.println("Store Name: " + this.storeName);
+        System.out.println("location: " + this.location + " " + this.state);
+        System.out.println("Result of the the Unionized Voting is: " + 
+                (this.results ? "yes, unionized" : "No, not unionized"));
     }
 }
